716122aebf07580c1768a4c9b84923fd7926713c
Tom Ritserveldt <muffin@inuits.be>
2016-07-25 08:47:53 +0200

    put defined resources in a seperate manifest - puppet4

--- manifests/vcl.pp
+++ manifests/vcl.pp
@@ -63,25 +63,6 @@ class varnish::vcl (
 
   include varnish
 
-  # define include file type
-  define includefile {
-    $selectors = $varnish::vcl::selectors
-    concat { "${varnish::vcl::includedir}/${title}.vcl":
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0444',
-      notify  => Service['varnish'],
-      require => File[$varnish::vcl::includedir],
-    }
-
-    concat::fragment { "${title}-header":
-      target  => "${varnish::vcl::includedir}/${title}.vcl",
-      content => "# File managed by Puppet\n",
-      order   => '01',
-    }
-  }
-
-
   # select template to use
   if $template {
     $template_vcl = $template
